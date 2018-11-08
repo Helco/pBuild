2100a4659a34f76130798d261359212166ba5de9
Helco <Helco@users.noreply.github.com>
2018-11-08 21:26:05 +0100

    Try some rebble changes

--- before_install.sh
+++ before_install.sh
@@ -3,18 +3,20 @@
 npm install npm@3 -g
 
 set -e
-echo 'pBuild 1.5'
+echo 'pBuild 1.6 - fork by Helco'
 echo 'Installing Pebble SDK and its Dependencies...'
 
 cd ~
 
 mkdir -p ~/pebble-dev
 mkdir -p ~/.pebble-sdk
-touch ~/.pebble-sdk/ENABLE_ANALYTICS
+touch ~/.pebble-sdk/NO_TRACKING
+
+export PEBBLE_SDK=pebble-sdk-4.5-linux64
 
 # Get the Pebble SDK and toolchain
-if [ ! -d $HOME/pebble-dev/${PEBBLE_SDK} ]; then
-  wget https://s3.amazonaws.com/assets.getpebble.com/pebble-tool/${PEBBLE_SDK}.tar.bz2
+if [ ! -d "$HOME/pebble-dev" ]; then
+  wget https://developer.rebble.io/s3.amazonaws.com/assets.getpebble.com/pebble-tool/${PEBBLE_SDK}.tar.bz2
 
   # Extract the SDK
   tar -jxf ${PEBBLE_SDK}.tar.bz2 -C ~/pebble-dev/
